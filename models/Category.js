84b61f9ce515b0cebbcaf16a2aa26d80153fbce5
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-07-08 01:43:52 +0100

    created category class

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-// Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
